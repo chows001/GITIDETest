b6e1a5c3c26e1e66e345dbd3b2c93ff84b7d523e
tom_glb <tom_glb@egoonet.com>
2021-10-24 19:57:41 +0800

    Add first Java class: "Hello Java!"

---
+++ HelloJava/src/com/chows/java/hello.java
@@ -0,0 +1,11 @@
+package com.chows.java;
+
+/**
+ * @author Chows
+ * @create 2021-10-24 19:55
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+    }
+}
